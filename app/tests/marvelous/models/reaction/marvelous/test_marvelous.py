ec12330bf92fb3b864589248854b60f6ff38d4b2
watano1168 <watano1168@gmail.com>
2021-04-14 11:44:24 +0900

    [Fix] marvelousモデルのテストを最適化

--- app/tests/marvelous/models/reaction/marvelous/test_marvelous.py
+++ app/tests/marvelous/models/reaction/marvelous/test_marvelous.py
@@ -1,104 +1,88 @@
 import pytest
 import pytest_mock
-import src.marvelous.models.reaction.marvelous.marvelous as target_package
 from src.marvelous.models.reaction.marvelous.marvelous import *
 from dataclasses import dataclass
+
+
+@pytest.fixture()
+def settings():
+    return MarvelousSettings(
+        daily_step_limit=10,
+        steps_per_bonus=5,
+        sender_bonus_point=1,
+        receiver_point=1
+    )
 
 
 @dataclass()
 class SendCase:
     # 入力パラメータ
-    sender_point: int
-    receiver_point: int
-    settings: MarvelousSettings
-
-    # ダミーパラーメータ
+    before_sender_point: int
+    before_receiver_point: int
+    after_sender_point: int
+    after_receiver_point: int
     bonus: int
-
-    # 出力パラメータ
-    expected_sender_point: int
-    expected_receiver_point: int
-    expected_result: MarvelousResult
+    expected_send_result: MarvelousResult
+    expected_cancel_result: MarvelousResult
 
 
-add_step_cases = {
-    "case0":
+send_cases = {
+    "no bonus":
         SendCase(
-            sender_point=0, receiver_point=0,
-            settings=MarvelousSettings(daily_step_limit=10, steps_per_bonus=5, sender_bonus_point=1, receiver_point=1),
+            before_sender_point=10, before_receiver_point=10,
+            after_sender_point=10, after_receiver_point=11,
             bonus=0,
-            expected_sender_point=0, expected_receiver_point=1,
-            expected_result=MarvelousResult(sender_bonus_diff=0, sender_point_diff=0, receiver_point_diff=1)
-        )
+            expected_send_result=MarvelousResult(sender_bonus_diff=0, sender_point_diff=0, receiver_point_diff=1),
+            expected_cancel_result=MarvelousResult(sender_bonus_diff=0, sender_point_diff=0, receiver_point_diff=-1)
+        ),
+    "with bonus":
+        SendCase(
+            before_sender_point=10, before_receiver_point=10,
+            after_sender_point=11, after_receiver_point=11,
+            bonus=1,
+            expected_send_result=MarvelousResult(sender_bonus_diff=1, sender_point_diff=1, receiver_point_diff=1),
+            expected_cancel_result=MarvelousResult(sender_bonus_diff=-1, sender_point_diff=-1, receiver_point_diff=-1)
+        ),
 }
 
 
-@pytest.mark.parametrize("case", add_step_cases.values(), ids=add_step_cases.keys())
-def test_send(case: SendCase, mocker: pytest_mock.MockerFixture):
+@pytest.mark.parametrize("case", send_cases.values(), ids=send_cases.keys())
+def test_send(case: SendCase, settings: MarvelousSettings, mocker: pytest_mock.MockerFixture):
     sender = mocker.Mock()
-    sender.point = case.sender_point
+    sender.point = case.before_sender_point
     sender.marvelous_bonus.add_step = mocker.Mock(return_value=case.bonus)
     receiver = mocker.Mock()
-    receiver.point = case.receiver_point
-    marvelous: MarvelousReaction = MarvelousReaction(case.settings)
+    receiver.point = case.before_receiver_point
+    marvelous: MarvelousReaction = MarvelousReaction(settings)
 
     marvelous.send(sender, receiver)
 
     sender.marvelous_bonus.add_step.assert_called_once_with(
         step=1,
-        today_limit=case.settings.daily_step_limit,
-        step_interval=case.settings.steps_per_bonus
+        today_limit=settings.daily_step_limit,
+        step_interval=settings.steps_per_bonus
     )
-    assert sender.point == case.expected_sender_point
-    assert receiver.point == case.expected_receiver_point
-    assert marvelous.result == case.expected_result
+    assert sender.point == case.after_sender_point
+    assert receiver.point == case.after_receiver_point
+    assert marvelous.result == case.expected_send_result
 
 
-
-@dataclass()
-class CancelCase:
-    # 入力パラメータ
-    sender_point: int
-    receiver_point: int
-    settings: MarvelousSettings
-
-    # ダミーパラーメータ
-    bonus: int
-
-    # 出力パラメータ
-    expected_sender_point: int
-    expected_receiver_point: int
-    expected_result: MarvelousResult
-
-
-add_step_cases = {
-    "case0":
-        CancelCase(
-            sender_point=0, receiver_point=1,
-            settings=MarvelousSettings(daily_step_limit=10, steps_per_bonus=5, sender_bonus_point=1, receiver_point=1),
-            bonus=0,
-            expected_sender_point=0, expected_receiver_point=0,
-            expected_result=MarvelousResult(sender_bonus_diff=0, sender_point_diff=0, receiver_point_diff=-1)
-        )
-}
-
-
-@pytest.mark.parametrize("case", add_step_cases.values(), ids=add_step_cases.keys())
-def test_cancel(case: CancelCase, mocker: pytest_mock.MockerFixture):
+@pytest.mark.parametrize("case", send_cases.values(), ids=send_cases.keys())
+def test_cancel(case: SendCase, settings: MarvelousSettings, mocker: pytest_mock.MockerFixture):
     sender = mocker.Mock()
-    sender.point = case.sender_point
-    sender.marvelous_bonus.add_step = mocker.Mock(return_value=case.bonus)
+    sender.point = case.after_sender_point
+    sender.marvelous_bonus.add_step = mocker.Mock(return_value=-case.bonus)
     receiver = mocker.Mock()
-    receiver.point = case.receiver_point
-    marvelous: MarvelousReaction = MarvelousReaction(case.settings)
+    receiver.point = case.after_receiver_point
+    marvelous: MarvelousReaction = MarvelousReaction(settings)
 
     marvelous.cancel(sender, receiver)
 
     sender.marvelous_bonus.add_step.assert_called_once_with(
         step=-1,
-        today_limit=case.settings.daily_step_limit,
-        step_interval=case.settings.steps_per_bonus
+        today_limit=settings.daily_step_limit,
+        step_interval=settings.steps_per_bonus
     )
-    assert sender.point == case.expected_sender_point
-    assert receiver.point == case.expected_receiver_point
-    assert marvelous.result == case.expected_result
+    assert sender.point == case.before_sender_point
+    assert receiver.point == case.before_receiver_point
+    assert marvelous.result == case.expected_cancel_result
