cb6d7908e5c45ed5d5c8ad4355e7bc63d838260b
watano1168 <watano1168@gmail.com>
2021-04-14 11:42:08 +0900

    [Fix] booingモデルのテストを最適化

--- app/tests/marvelous/models/reaction/booing/test_booing.py
+++ app/tests/marvelous/models/reaction/booing/test_booing.py
@@ -1,104 +1,88 @@
 import pytest
 import pytest_mock
-import src.marvelous.models.reaction.booing.booing as target_package
 from src.marvelous.models.reaction.booing.booing import *
 from dataclasses import dataclass
+
+
+@pytest.fixture()
+def settings():
+    return BooingSettings(
+        daily_step_limit=10,
+        steps_per_bonus=5,
+        sender_bonus_point=-1,
+        receiver_point=-1
+    )
 
 
 @dataclass()
 class SendCase:
     # 入力パラメータ
-    sender_point: int
-    receiver_point: int
-    settings: BooingSettings
-
-    # ダミーパラーメータ
+    before_sender_point: int
+    before_receiver_point: int
+    after_sender_point: int
+    after_receiver_point: int
     penalty: int
-
-    # 出力パラメータ
-    expected_sender_point: int
-    expected_receiver_point: int
-    expected_result: BooingResult
+    expected_send_result: BooingResult
+    expected_cancel_result: BooingResult
 
 
-add_step_cases = {
-    "case0":
+send_cases = {
+    "no penalty":
         SendCase(
-            sender_point=0, receiver_point=1,
-            settings=BooingSettings(daily_step_limit=10, steps_per_bonus=5, sender_bonus_point=-1, receiver_point=-1),
+            before_sender_point=10, before_receiver_point=10,
+            after_sender_point=10, after_receiver_point=9,
             penalty=0,
-            expected_sender_point=0, expected_receiver_point=0,
-            expected_result=BooingResult(sender_penalty_diff=0, sender_point_diff=0, receiver_point_diff=-1)
-        )
+            expected_send_result=BooingResult(sender_penalty_diff=0, sender_point_diff=0, receiver_point_diff=-1),
+            expected_cancel_result=BooingResult(sender_penalty_diff=0, sender_point_diff=0, receiver_point_diff=1)
+        ),
+    "with penalty":
+        SendCase(
+            before_sender_point=10, before_receiver_point=10,
+            after_sender_point=9, after_receiver_point=9,
+            penalty=1,
+            expected_send_result=BooingResult(sender_penalty_diff=1, sender_point_diff=-1, receiver_point_diff=-1),
+            expected_cancel_result=BooingResult(sender_penalty_diff=-1, sender_point_diff=1, receiver_point_diff=1)
+        ),
 }
 
 
-@pytest.mark.parametrize("case", add_step_cases.values(), ids=add_step_cases.keys())
-def test_send(case: SendCase, mocker: pytest_mock.MockerFixture):
+@pytest.mark.parametrize("case", send_cases.values(), ids=send_cases.keys())
+def test_send(case: SendCase, settings: BooingSettings, mocker: pytest_mock.MockerFixture):
     sender = mocker.Mock()
-    sender.point = case.sender_point
+    sender.point = case.before_sender_point
     sender.booing_penalty.add_step = mocker.Mock(return_value=case.penalty)
     receiver = mocker.Mock()
-    receiver.point = case.receiver_point
-    booing: BooingReaction = BooingReaction(case.settings)
+    receiver.point = case.before_receiver_point
+    booing: BooingReaction = BooingReaction(settings)
 
     booing.send(sender, receiver)
 
     sender.booing_penalty.add_step.assert_called_once_with(
         step=1,
-        today_limit=case.settings.daily_step_limit,
-        step_interval=case.settings.steps_per_bonus
+        today_limit=settings.daily_step_limit,
+        step_interval=settings.steps_per_bonus
     )
-    assert sender.point == case.expected_sender_point
-    assert receiver.point == case.expected_receiver_point
-    assert booing.result == case.expected_result
+    assert sender.point == case.after_sender_point
+    assert receiver.point == case.after_receiver_point
+    assert booing.result == case.expected_send_result
 
 
-
-@dataclass()
-class CancelCase:
-    # 入力パラメータ
-    sender_point: int
-    receiver_point: int
-    settings: BooingSettings
-
-    # ダミーパラーメータ
-    penalty: int
-
-    # 出力パラメータ
-    expected_sender_point: int
-    expected_receiver_point: int
-    expected_result: BooingResult
-
-
-add_step_cases = {
-    "case0":
-        CancelCase(
-            sender_point=0, receiver_point=0,
-            settings=BooingSettings(daily_step_limit=10, steps_per_bonus=5, sender_bonus_point=-1, receiver_point=-1),
-            penalty=0,
-            expected_sender_point=0, expected_receiver_point=1,
-            expected_result=BooingResult(sender_penalty_diff=0, sender_point_diff=0, receiver_point_diff=1)
-        )
-}
-
-
-@pytest.mark.parametrize("case", add_step_cases.values(), ids=add_step_cases.keys())
-def test_cancel(case: CancelCase, mocker: pytest_mock.MockerFixture):
+@pytest.mark.parametrize("case", send_cases.values(), ids=send_cases.keys())
+def test_cancel(case: SendCase, settings: BooingSettings, mocker: pytest_mock.MockerFixture):
     sender = mocker.Mock()
-    sender.point = case.sender_point
-    sender.booing_penalty.add_step = mocker.Mock(return_value=case.penalty)
+    sender.point = case.after_sender_point
+    sender.booing_penalty.add_step = mocker.Mock(return_value=-case.penalty)
     receiver = mocker.Mock()
-    receiver.point = case.receiver_point
-    booing: BooingReaction = BooingReaction(case.settings)
+    receiver.point = case.after_receiver_point
+    booing: BooingReaction = BooingReaction(settings)
 
     booing.cancel(sender, receiver)
 
     sender.booing_penalty.add_step.assert_called_once_with(
         step=-1,
-        today_limit=case.settings.daily_step_limit,
-        step_interval=case.settings.steps_per_bonus
+        today_limit=settings.daily_step_limit,
+        step_interval=settings.steps_per_bonus
     )
-    assert sender.point == case.expected_sender_point
-    assert receiver.point == case.expected_receiver_point
-    assert booing.result == case.expected_result
+    assert sender.point == case.before_sender_point
+    assert receiver.point == case.before_receiver_point
+    assert booing.result == case.expected_cancel_result
