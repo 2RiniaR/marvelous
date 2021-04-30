9d739160c73ffda4b58cd149f3f4279a660338b6
watano1168 <watano1168@gmail.com>
2021-04-30 23:53:29 +0900

    [Add] ヘルプ表示の改善

--- app/src/marvelous/client/discord/help.py
+++ app/src/marvelous/client/discord/help.py
@@ -1,15 +1,57 @@
 from discord.ext import commands
+import discord
+from logging import getLogger
+
+
+logger = getLogger(__name__)
 
 
 class MarvelousHelpCommand(commands.DefaultHelpCommand):
     def __init__(self):
         super().__init__()
         self.commands_heading = "コマンド:"
-        self.no_category = "その他"
+        self.no_category = ""
         self.command_attrs["help"] = "コマンド一覧とヘルプを表示する"
+        self.content = (
+            "各ユーザーの 👏えらいポイント を管理するbotです。\n"
+            "\n"
+            "他のユーザーのメッセージに以下のリアクションを付けると、アクションが可能です。\n"
+            "```md\n"
+            "👏 「えらい！」を送る\n"
+            "    - 相手に👏+1\n"
+            "🖕 「カス！」を送る\n"
+            "    - 相手に👏-1\n"
+            "🙌 「めっちゃえらい！」を送る\n"
+            "    - 相手に👏+10\n"
+            "    - 自分に👏+1\n"
+            "    - 各ユーザー、1週間に3回まで（毎週日曜4:00に回数リセット）\n"
+            "```\n"
+            "\n"
+            "その他、以下のアクションによって 👏えらいポイント が変動します。\n"
+            "```md\n"
+            "その日最初のメッセージを送る\n"
+            "    - 自分に👏+1\n"
+            "「えらい！」を5回送る（10カウント/日）\n"
+            "    - 自分に👏+1\n"
+            "「カス！」を5回送る（10カウント/日）\n"
+            "    - 自分に👏-1\n"
+            "```\n"
+            "\n"
+            "【コマンド一覧】\n"
+            "`!erai me`      : 自分のステータスを表示する\n"
+            "`!erai ranking` : えらいポイントのランキングを表示する\n"
+            "`!erai help`    : ヘルプを表示する\n"
+        )
 
     def get_ending_note(self):
         return ""
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="エライさんbot - ヘルプ", description=self.content, color=0x00ff00)
+        try:
+            await self.get_destination().send(embed=embed)
+        except Exception as err:
+            logger.error(str(err))
 
     def command_not_found(self, string):
         return f"{string} というコマンドは存在しません。"
--- bin/logs.sh
+++ bin/logs.sh
@@ -3,4 +3,4 @@
 cd `dirname $0`
 cd ..
 
-docker-compose logs
+docker-compose logs -tf
