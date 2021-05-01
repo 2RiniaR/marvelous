84fd059384f8567d426ace89f4a91e6f6d245c71
watano1168 <watano1168@gmail.com>
2021-05-02 03:41:20 +0900

    [Add] えらいポイントをリセットするデバッグコマンドを追加

--- app/src/marvelous/client/discord/debug.py
+++ app/src/marvelous/client/discord/debug.py
@@ -1,26 +1,31 @@
 from discord.ext import commands
-from ..presentation import run_reset_super_marvelous_left, run_reset_survival_bonus, run_reset_daily_steps
+from ..presentation import (
+    run_reset_super_marvelous_left, run_reset_survival_bonus, run_reset_daily_steps, run_reset_marvelous_point
+)
 
 
 @commands.command()
 async def reset_survival_bonus(ctx: commands.Context):
-    """👤自分のステータスを表示する"""
     run_reset_survival_bonus()
 
 
 @commands.command()
 async def reset_super_marvelous_left(ctx: commands.Context):
-    """👑ランキングを表示する"""
     run_reset_super_marvelous_left()
 
 
 @commands.command()
 async def reset_daily_steps(ctx: commands.Context):
-    """👑ランキングを表示する"""
     run_reset_daily_steps()
+
+
+@commands.command()
+async def reset_marvelous_point(ctx: commands.Context):
+    run_reset_marvelous_point()
 
 
 def setup(bot: commands.Bot):
     bot.add_command(reset_survival_bonus)
     bot.add_command(reset_super_marvelous_left)
     bot.add_command(reset_daily_steps)
+    bot.add_command(reset_marvelous_point)
