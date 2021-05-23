1dd12ced5f12db4dc2b7b0639d0dcd015b1859c6
watano1168 <watano1168@gmail.com>
2021-05-24 00:39:39 +0900

    デバッグコマンド実行時にメッセージを送信するように変更

--- app/src/marvelous/client/discord/debug.py
+++ app/src/marvelous/client/discord/debug.py
@@ -5,26 +5,31 @@ from .. import presentation
 @commands.command()
 async def reset_survival_bonus(ctx: commands.Context):
     presentation.run_reset_survival_bonus()
+    await ctx.channel.send("[DEBUG RUN] Reset survival bonus")
 
 
 @commands.command()
 async def reset_super_marvelous_left(ctx: commands.Context):
     presentation.run_reset_super_marvelous_left()
+    await ctx.channel.send("[DEBUG RUN] Reset super marvelous left")
 
 
 @commands.command()
 async def reset_daily_steps(ctx: commands.Context):
     presentation.run_reset_daily_steps()
+    await ctx.channel.send("[DEBUG RUN] Reset daily steps")
 
 
 @commands.command()
 async def reset_marvelous_point(ctx: commands.Context):
     await presentation.run_reset_marvelous_point()
+    await ctx.channel.send("[DEBUG RUN] Reset marvelous point")
 
 
 @commands.command()
 async def check_github_bonus(ctx: commands.Context):
     presentation.run_github_bonus()
+    await ctx.channel.send("[DEBUG RUN] Check GitHub bonus")
 
 
 def setup(bot: commands.Bot):
