75804aba820b57fb7af1653862f4c2d173c7ecf0
watano1168 <watano1168@gmail.com>
2021-05-23 17:41:03 +0900

    新しいコマンドの追加

--- app/src/marvelous/client/discord/commands.py
+++ app/src/marvelous/client/discord/commands.py
@@ -12,6 +12,23 @@ async def ranking(ctx: commands.Context):
     await presentation.show_ranking(ctx.channel)
 
 
+@commands.group()
+async def github(ctx: commands.Context):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('このコマンドにはサブコマンドが必要です。')
+
+
+@github.command(name="register")
+async def register_github(ctx: commands.Context, github_id: str):
+    await presentation.register_github(ctx.author, ctx.channel, github_id)
+
+
+@github.command(name="unregister")
+async def unregister_github(ctx: commands.Context):
+    await presentation.unregister_github(ctx.author, ctx.channel)
+
+
 def setup(bot: commands.Bot):
     bot.add_command(me)
     bot.add_command(ranking)
+    bot.add_command(github)
